f5ab66d2348d7313d8b651ca5271770d8b8d0a4c
Thomas Delcros <delcrosthomas@gmail.com>
2018-09-28 15:18:12 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :events, only: [:index]
+  resources :events, only: [:index, :show]
 
   namespace :admin do
     resources :events, only: [:index]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_13_092303) do
+ActiveRecord::Schema.define(version: 2018_09_28_111104) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_09_13_092303) do
     t.boolean "out_of_places", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
   end
 
   create_table "participations", force: :cascade do |t|
