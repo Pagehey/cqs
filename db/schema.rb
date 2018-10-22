4f6977f08226038b0d35f92a18ca1f99ec818e63
Pierre Tissot <pierregabriel@me.com>
2018-10-22 20:40:44 +0200

    working on unread

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_28_111104) do
+ActiveRecord::Schema.define(version: 2018_10_22_171130) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_09_28_111104) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.integer "price"
-    t.string "number_of_places"
+    t.integer "number_of_places"
     t.string "category"
     t.boolean "out_of_places", default: false
     t.datetime "created_at", null: false
@@ -41,6 +41,17 @@ ActiveRecord::Schema.define(version: 2018_09_28_111104) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["event_id"], name: "index_participations_on_event_id"
+  end
+
+  create_table "read_marks", id: :serial, force: :cascade do |t|
+    t.string "readable_type", null: false
+    t.integer "readable_id"
+    t.string "reader_type", null: false
+    t.integer "reader_id"
+    t.datetime "timestamp"
+    t.index ["readable_type", "readable_id"], name: "index_read_marks_on_readable_type_and_readable_id"
+    t.index ["reader_id", "reader_type", "readable_type", "readable_id"], name: "read_marks_reader_readable_index", unique: true
+    t.index ["reader_type", "reader_id"], name: "index_read_marks_on_reader_type_and_reader_id"
   end
 
   create_table "users", force: :cascade do |t|
