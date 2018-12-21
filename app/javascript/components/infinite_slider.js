3ef90246cc5c4bec6c9ef64c8072bd7ce313d15b
Pierre Tissot <pierregabriel@me.com>
2018-12-21 19:34:00 +0000

    WIP

--- app/javascript/components/infinite_slider.js
+++ app/javascript/components/infinite_slider.js
@@ -1,77 +1,79 @@
 if (document.querySelector('.slider')) {
-  let   slides          = getSlides();
-  const slidesNumber    = slides.length;
-  const slidesContainer = document.querySelector('.slider');
+  const   infiniteSlider  = document.getElementById('infinite--slider')
+  const   initialSlides   = getSlides();
+  let     currentSlides   = getSlides();
+  const   slidesContainer = document.querySelector('.slider');
 
-  // if (slidesNumber > 2) { slides[2].classList.add('before-last-right') };
+  initialize();
 
-  // let lastSlide       = slides[slidesNumber - 1].cloneNode(true);
-  // let beforeLastSlide = slides[slidesNumber - 2].cloneNode(true);
-
-  // [lastSlide, beforeLastSlide].forEach(slide => {
-  //   slide.classList.remove('before-last-right')
-  // });
-
-  // beforeLastSlide.classList.add('before-last-left');
-
-  // slidesContainer.prepend(lastSlide);
-  // slidesContainer.prepend(beforeLastSlide);
-
-  let slidesArray = Array.from(slides);
-
-  slidesArray.reverse().forEach((slide, index) => {
-    let newSlide = slide.cloneNode(true);
-    newSlide.classList.remove('active');
-    console.log(newSlide)
-    slidesContainer.prepend(newSlide);
-  })
-
-  getSlides()[0].classList.add('before-last-left')
-  getSlides()[getSlides().length -1].classList.add('before-last-right')
+  function initialize() {
+    if (initialSlides.length > 1) {
+      getSlides().reverse().forEach((slide, index) => {
+        let newSlide = slide.cloneNode(true)
+        if (index != 0) newSlide.classList.add('far-left');
+        slidesContainer.prepend(newSlide);
+        if (index <= initialSlides.length - 3) slide.classList.add('far-right');
+      })
+    }
+    if (initialSlides.length == 2) {
+      initialSlides.forEach((slide, index) => {
+        let newSlide = slide.cloneNode(true)
+        newSlide.classList.add('far-right');
+        slidesContainer.appendChild(newSlide);
+      })
+    }
+    if (initialSlides.length == 1) {
+      document.querySelector('.slider--right').remove()
+      document.querySelector('.slider--left') .remove()
+    } else {
+      document.querySelector('.slider--right').addEventListener('click', ()=>{next()})
+      document.querySelector('.slider--left') .addEventListener('click', ()=>{previous()})
+    }
+    initialSlides[0].classList.add('active')
+    currentSlides = getSlides();
+    document.addEventListener('DOMContentLoaded', ()=>{slidesContainer.classList.add('initialized')})
+  }
 
   function previous() {
-    let currentSlides = getSlides()
-    let slideToMove   = currentSlides[currentSlides.length - 1].cloneNode(true)
+    let slideToMove   = getSlides('last')
     let activeSlide   = document.querySelector('.slide.active')
 
-    slideToMove.classList.remove('before-last-right')
-    slideToMove.classList.add('before-last-left')
+    slideToMove.classList.replace('far-right', 'far-left')
     slidesContainer.prepend(slideToMove)
 
-    currentSlides[currentSlides.length - 1].remove()
-    getSlides()[getSlides().length - 1].classList.add('before-last-right')
-    getSlides()[1].classList.remove('before-last-left')
-
     activeSlide.classList.remove('active')
-    activeSlide.nextSibling.classList.remove('right-sibling')
-    activeSlide.classList.add('right-sibling')
 
-    activeSlide.previousSibling.classList.add('active')
+    activeSlide.previousSibling.classList.add('active');
+    activeSlide.nextSibling.classList.add('far-right')
+    document.querySelector('.slide.active').previousSibling.classList.remove('far-left');
   };
 
   function next() {
-    let currentSlides = getSlides()
-    let slideToMove = currentSlides[0].cloneNode(true)
+    let slideToMove = getSlides('first')
     let activeSlide = document.querySelector('.slide.active')
 
-    slideToMove.classList.remove('before-last-left')
-    slideToMove.classList.add('before-last-right')
+    slideToMove.classList.replace('far-left', 'far-right')
     slidesContainer.appendChild(slideToMove)
-
-    currentSlides[0].remove()
-    currentSlides[0].classList.add('before-last-left')
 
     activeSlide.classList.remove('active')
 
     activeSlide.nextSibling.classList.add('active')
-    activeSlide.nextSibling.nextSibling.classList.remove('before-last-right')
+    activeSlide.previousSibling.classList.add('far-left')
+    document.querySelector('.slide.active').nextSibling.classList.remove('far-right')
   };
 
-  function getSlides() {
-    return document.querySelectorAll('.slide')
+  function getSlides(index = null) {
+    const slides =  Array.from(document.querySelectorAll('.slide'))
+    if (index == 'first') {
+      return slides[0]
+    } else if (index == 'last') {
+      return slides[slides.length - 1]
+    } else {
+      return slides
+    }
   };
 
-  setInterval(()=> { previous() }, 2000);
-  // setTimeout(()=> { previous() }, 2000);
-  // setInterval(()=> { next() }, 2000);
+  let autoNext = setInterval(()=>{next()}, 5000);
+  infiniteSlider.addEventListener('mouseenter', ()=>{ clearInterval(autoNext) });
+  infiniteSlider.addEventListener('mouseleave', ()=>{ autoNext = setInterval(()=>{next()}, 5000) });
 };
