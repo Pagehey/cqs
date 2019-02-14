3c1a1df34a75f3046c0656759f0309b79f6e4935
Pierre Tissot <pierregabriel@me.com>
2019-02-14 11:49:07 +0100

    add force ssl and not event guard on home

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
 # skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @first_three_events = Event.order(:start_date).first(3)
   end
 
   def permaculture
