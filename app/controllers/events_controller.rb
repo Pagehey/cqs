e4fcdaba49c168326440bc229d21ce5a352a7c2f
Pagehey <pierregabriel@me.com>
2019-03-07 15:14:59 +0100

    fix meta_tags order participations 404 for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,11 +1,9 @@
 class EventsController < ApplicationController
   before_action :set_event, only: :show
 
-  def index
-  end
-
   def show
     @participation = Participation.new
+    render :event_not_found if @event.out_of_places || @event.end_date < Date.today
   end
 
   private
